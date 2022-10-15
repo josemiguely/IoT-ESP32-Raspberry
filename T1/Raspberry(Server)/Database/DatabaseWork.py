73d7fd900d95fe5d6be67f9849918aaf27bc4c0a
Stevennns <stevens.egli13@gmail.com>
2022-10-15 19:38:53 -0300

    Protocol 0 ready

--- T1/Raspberry(Server)/Database/DatabaseWork.py
+++ T1/Raspberry(Server)/Database/DatabaseWork.py
@@ -1,5 +1,6 @@
 import json
 import sqlite3 as sql
+import os
 
 # Documentación https://docs.python.org/3/library/sqlite3.html
 
@@ -8,8 +9,10 @@ import sqlite3 as sql
 def dataSave(header, data):
     protocol=header["protocol"]
 
+    path=os.getcwd()
+    print(f" EL PATH PARA ABRIR ESTA EN {path}")
     try:
-        sqliteConnection = sql.connect('DB.sqlite')
+        sqliteConnection = sql.connect('../Database/DB.sqlite')
         cursor = sqliteConnection.cursor()
     except Exception:
         print("No se logro conectar a la base de datos")
@@ -20,36 +23,36 @@ def dataSave(header, data):
         # Tabla Datos
         try:
 
-            sqlite_insert_with_param = """INSERT INTO Datos (MessageId,
-    IdDevice,
-    MAC,
-    Timestamp,
-    Data1,
-    Data2,
-    Data3,
-    Data4,
-    Data5,
-    Data6,
-    Data7,
-    Data8,
-    Data9,
-    Data10,
-    Data11,
-    Data12,
-    Data13,
-    Data14,
-    Data15,
-    Data16,
-    Data17) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+            sqlite_insert_with_param = """
+            INSERT INTO Datos (
+            IdDevice,
+            MAC,
+            Timestamp,
+            Data1,
+            Data2,
+            Data3,
+            Data4,
+            Data5,
+            Data6,
+            Data7,
+            Data8,
+            Data9,
+            Data10,
+            Data11,
+            Data12,
+            Data13,
+            Data14,
+            Data15,
+            Data16,
+            Data17) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
             ## Poner la configuracion segun el parser
             print("DATAAAAAA = ",end=" ")
             print(data)
             data_tuple = (header["ID_Dev"], header["MAC"],data["Timestamp"],data["Val"], data["Batt"],data["Timestamp"], None, None, None, None, None, None, None, None, None, None, None, None, None, None) # -> Corregir
-            cursor.excecute(sqlite_insert_with_param, data_tuple)
+            cursor.execute(sqlite_insert_with_param, data_tuple)
             sqliteConnection.commit()
-            cursor.close
         except sql.Error as error:
-            print("Failed to insert Python variable into sqlite table", error)
+            print("Error al insertar en protocolo 0",error)
         finally:
             if sqliteConnection:
                 sqliteConnection.close()
@@ -122,18 +125,18 @@ def dataSave(header, data):
 
 
     # Tabla Logs (Para todos)
-    try:
+    # try:
         
-        cursor = sqliteConnection.cursor()
+    #     cursor = sqliteConnection.cursor()
         
-        sqlite_insert_with_param = """INSERT INTO Datos (MessageId, IdDevice, MAC, Timestamp, Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data15, Data16, Data17) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
-        ## Rellenar con los nombres segun el parser
-        data_tuple = ()
-        cursor.execute(sqlite_insert_with_param, data_tuple)
-        sqliteConnection.coomit()
-        cursor.close()
-    except sql.Error as error:
-        print("Failed to insert variable into sqlite table", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
+    #     sqlite_insert_with_param = """INSERT INTO Datos (MessageId, IdDevice, MAC, Timestamp, Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data15, Data16, Data17) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+    #     ## Rellenar con los nombres segun el parser
+    #     data_tuple = ()
+    #     cursor.execute(sqlite_insert_with_param, data_tuple)
+    #     sqliteConnection.coomit()
+    #     cursor.close()
+    # except sql.Error as error:
+    #     print("Failed to insert variable into sqlite table", error)
+    # finally:
+    #     if sqliteConnection:
+    #         sqliteConnection.close()
